482b43be3495870de3c403ff9ae6ea5500e1ff27
BenTGI <126521168+BenTGI@users.noreply.github.com>
2023-02-27 21:40:34 +0000

    Add files via upload

---
+++ ModEntry.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Monsters;
+using StardewValley.Tools;
+
+namespace YourProjectName
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+           
+
+        }
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if (e.Button == SButton.L)
+                Monitor.Log($"wow, my health is {Game1.player.health}", LogLevel.Debug);
+                Monitor.Log($"wow, my max health is {Game1.player.maxHealth}", LogLevel.Debug);
+        }
+
+        
+
+
+    }
+    
+}
---
+++ Playerdead.cs
@@ -0,0 +1,29 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Monsters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace One_Hit_KO_Mod
+{
+    public  class Playerdead
+    {
+        private static void takeDamage_Postfix()
+        {
+            try
+            { 
+                takeDamage(int damage, bool overrideParry,"die", Monster damager)
+            }
+                catch (Exception ex)
+                {
+                  
+                }
+            
+        }
+    }
+}
